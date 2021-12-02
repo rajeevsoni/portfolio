94c353eb14ec1d8f4427b1d27a644c7e26afbc0a
Rajeev Soni <Rajeev_Soni@mckinsey.com>
2021-12-02 23:11:10 +0530

    initial changes

---
+++ src/component/about.jsx
@@ -0,0 +1,134 @@
+import React from "react";
+import layers from "../img/layers.png";
+import cogwheel from "../img/cogwheel.png";
+import pen from "../img/pen.png";
+import browser from "../img/browser.png";
+import myImage from "../img/myImage.JPG";
+import PropTypes from "prop-types";
+
+export default class About extends React.Component {
+	render() {
+		return (
+			<div
+				id="about-container"
+				className="content-containers container text-center mt-5">
+				<h1 id="about" className={this.props.bounceLeft}>
+					About
+				</h1>
+				<div className="row" style={{ marginTop: 7 + "rem" }}>
+					<div className="col-12 col-lg-6 hidden">
+						<img
+							className={"img-fluid " + this.props.fadeIn}
+							src={myImage}
+							alt=""
+							style={{
+								borderRadius: 50 + "%",
+								height: 250 + "px",
+								width: 250 + "px"
+							}}
+						/>
+						<p className={"mytext pt-3 " + this.props.fadeIn}>
+							Hello, I am Rajeev Soni. I am a Software Engineer based out of India.
+							I have interest in distributed systems and system design.
+							I love playing table tennis and lawn tennis. I also play piano in my free time.
+						</p>
+					</div>
+					<div
+						className={
+							"col-12 col-sm-12 col-md-12 col-lg-6 " +
+							this.props.tada
+						}>
+						<div className="progress mb-3">
+							<div
+								className="progress-bar fill-80-bar"
+								role="progressbar"
+								style={{ width: 90 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								.NET Core
+							</div>
+						</div>
+						<div className="progress mb-3">
+							<div
+								className="progress-bar fill-80-bar"
+								role="progressbar"
+								style={{ width: 90 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								C#
+							</div>
+						</div>
+						<div className="progress mb-3">
+							<div
+								className="progress-bar fill-90-bar"
+								role="progressbar"
+								style={{ width: 80 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								Javascript
+							</div>
+						</div>
+						<div className="progress mb-3">
+							<div
+								className="progress-bar fill-60-bar"
+								role="progressbar"
+								style={{ width: 80 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								SQL
+							</div>
+						</div>
+						<div className="progress mb-3">
+							<div
+								className="progress-bar fill-80-bar"
+								role="progressbar"
+								style={{ width: 80 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								ReactJS
+							</div>
+						</div>
+						<div className="progress mb-3">
+							<div
+								className="progress-bar fill-60-bar"
+								role="progressbar"
+								style={{ width: 60 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								NodeJs
+							</div>
+						</div>
+						<div className="progress mb-3">
+							<div
+								className="progress-bar fill-80-bar"
+								role="progressbar"
+								style={{ width: 70 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								Docker & Kubernetes
+							</div>
+						</div>
+						<div className="progress mb-3">
+							<div
+								className="progress-bar fill-60-bar"
+								role="progressbar"
+								style={{ width: 80 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								Cassandra
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+About.propTypes = {
+	bounceLeft: PropTypes.string,
+	fadeInLeft: PropTypes.string,
+	fadeInRight: PropTypes.string,
+	fadeIn: PropTypes.string,
+	tada: PropTypes.string
+};
